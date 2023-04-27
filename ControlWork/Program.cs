391968891e8db4728494e3ef7b8e0763a3b77b9e
Nikita Tsygankov <like_fender@mail.ru>
2023-04-27 15:48:38 +0300

    interface change added

--- ControlWork/Program.cs
+++ ControlWork/Program.cs
@@ -1,4 +1,52 @@
 ﻿// program to convert array
 Console.WriteLine("Hello, what`s your name?");
-Console.ReadLine();
-Console.WriteLine("Hello, {user}");
+string username = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Hello,");
+Console.Write(username);
+Console.WriteLine();
+Console.WriteLine();
+
+
+Console.Write("Enter number of arrays elements: ");
+int m = int.Parse(Console.ReadLine());
+string [] stringArray = new string [m];
+void array(string [] stringArray)
+{
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+     Console.WriteLine($"Enter {i+1} arrays element");
+     stringArray[i] = Console.ReadLine();
+  }
+}
+string [] symbol(string [] stringArray)
+{
+  int n = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    n++;
+  }
+  string [] rez = new string [n];
+  int j = 0;
+  for (int i = 0;i<stringArray.Length;i++)
+  {
+    if(stringArray[i].Length <=3)
+    {
+        rez[j] = stringArray[i];
+        j++;
+    }
+  }
+  return rez;
+}
+void printA(string [] stringArray)
+{
+    Console.Write("[");
+    for (int i = 0;i<stringArray.Length;i++)
+    {
+    Console.Write($"‘{stringArray[i]}’, ");
+    }
+    Console.Write("]");
+}
+array(stringArray);
+printA(symbol(stringArray));
